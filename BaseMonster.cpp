7b5d7c0772608bc1bfef970708c6073309255d15
DeBroglie99 <43996310+DeBroglie99@users.noreply.github.com>
2019-06-10 16:33:01 +0800

    血条 判断人物类型不太懂 20分钟后我还会回来的 彭劳斯先用着

---
+++ BaseMonster.cpp
@@ -0,0 +1,391 @@
+#include "BaseMonster.h"
+#include "SimpleAudioEngine.h"
+#include "BaseMap.h"
+#include "GameManager.h"
+
+USING_NS_CC;
+
+BaseMonster::BaseMonster()
+	:baseSprite(NULL),//啥
+	pointCounter(0),//这是个啥
+	runSpeed(0)
+	, maxHp(0)
+	, currHp(0)
+	, armor(0)
+	, hpPercentage(100)
+	, hpBar(NULL)
+	, lastState(stateNone)
+	//, attackBySoldier(true)//这俩
+	//, attackByTower(true)//应该能合并
+	, isAttacking(false)
+	, isSlowed(false)
+	, money(0)
+	, usualHpAdd(1)  //姑且先加血量加1吧 小老弟们有空了可以实现一下不同英雄加血速度不同以及装备加血的功能 
+	, homeHpAdd(10)
+{}
+
+BaseMonster::~BaseMonster()
+{}
+
+bool BaseMonster::init()
+{
+	if (!Sprite::init())
+	{
+		return false;
+	}
+	return true;
+}
+
+
+void BaseMonster::createAndSetHpBar()
+{
+	hpBgSprite = Sprite::createWithSpriteFrameName("");//好东西！之前见过的！！！哪见的不记得了嘤嘤嘤
+
+	hpBgSprite->setPosition(Point(baseSprite->getContentSize().width / 2, baseSprite->getContentSize().height));
+	baseSprite->addChild(hpBgSprite);
+
+	hpBar = ProgressTimer::create(Sprite::createWithSpriteFrameName(""));
+	hpBar->setType(ProgressTimer::Type::BAR);
+	hpBar->setMidpoint(Point(0, 0.5f));//开始点 左中？
+	hpBar->setBarChangeRate(Point(1, 0));//水平方向展开
+	hpBar->setPercentage(hpPercentage);
+	hpBar->setPosition(Point(hpBgSprite->getContentSize().width / 2, hpBgSprite->getContentSize().height / 2));
+	hpBar->setTag(666);  //加了个标签 
+	hpBgSprite->addChild(hpBar);
+	if(getmonsterType()== ) //这欠缺好多啊 他的type从哪儿来的啊  whatever再说吧
+	{
+		 schedule(schedule_selector(BaseMonster::hpSchedule),0.1f); 
+	} 
+	
+}
+
+void BaseMonster::Home()
+{
+	//加一下效果 
+	//延迟一下时间 
+	this->setPosition( , )//基地坐标 
+}
+
+void BaseMonster:: hpSchedule(float dt)
+{
+	ProgressTimer* _hpBar = (ProgrssTimer*)getChildByTag(666);
+	if(_hpBar->getPercentage()<100)
+	{
+		Vec2 tempos = this->getPosition();
+		if(tempos.x>=  &&tempos.x<=  &&tempos.y>=   &&tempos.y<=  )
+		{
+			_hpBar->setPercentage(_hpBar->getPercentage()+homeHpAdd);
+		}
+		else
+		{
+			_hpBar->setPercentage(_hpBar->getPercentage()+usualHpAdd);
+		}
+	}
+} 
+/*bool BaseMonster::beAttacked(BaseMonster* attacker)
+{
+	if (attacker->getMonsterColor() == this->getMonsterColor)
+	{
+		return false;
+	}
+	else
+		return true;
+}
+*/
+
+
+void BaseMonster::checkNearestMonster()
+{
+	auto instance = GameManager::getInstance();
+	if (this->getMonsterColor() == 1)
+	{
+		auto monsterBlueVector = instance->monsterBlueVector;
+
+		auto currMinDistant = this->scope;
+
+
+		nearestMonster = NULL;
+		for (int i = 0; i < monsterBlueVector.size(); i++)
+		{
+			auto monster = monsterBlueVector.at(i);
+			double distance = this->baseSprite->getPosition().getDistance(monster->baseSprite->getPosition());//去掉了getParent() 加了baseSprite 应该是这样吧虚虚虚
+
+			if (distance < currMinDistant&&monster->getState() != 7)//在塔的范围内且可以被攻击
+			{
+				currMinDistant = distance;//攻击最近怪物
+				monsterTemp = monster;//目前攻击的怪物
+			}
+		}
+		nearestMonster = monsterTemp;
+	}
+	else
+	{
+		auto monsterRedVector = instance->monsterRedVector;
+
+
+		auto currMinDistant = this->scope;
+
+
+		nearestMonster = NULL;
+		for (int i = 0; i < monsterRedVector.size(); i++)
+		{
+			auto monster = monsterRedVector.at(i);
+			double distance = this->baseSprite->getPosition().getDistance(monster->baseSprite->getPosition());
+
+			if (distance < currMinDistant&&monster->getState() != 7)//在塔的范围内且可以被攻击
+			{
+				currMinDistant = distance;//攻击最近怪物
+				monsterTemp = monster;//目前攻击的怪物
+			}
+		}
+		nearestMonster = monsterTemp;
+	}
+}
+
+void BaseMonster::death()
+{
+	if (getMonsterColor() == 1)
+	{
+		if (GameManager::getInstance()->monsterRedVector.contains(this))
+			GameManager::getInstance()->monsterRedVector.eraseObject(this);
+	}
+	else if (getMonsterColor() == 2)
+	{
+		if (GameManager::getInstance()->monsterBlueVector.contains(this))
+			GameManager::getInstance()->monsterBlueVector.eraseObject(this);//从vector移除
+	}
+	if (getState() != stateDeath) {
+		setCurrHp(0);
+		setState(stateDeath);
+		hpBgSprite->setVisible(false);
+		baseSprite->stopAllActions();
+		unscheduleUpdate();
+		//auto decal_blood = Sprite::createWithSpriteFrameName("decal_blood_0001.png");
+		//decal_blood->setPosition(Point(baseSprite->getContentSize().width / 2, -baseSprite->getContentSize().height / 4));
+		//baseSprite->addChild(decal_blood);
+		//GameManager::getInstance()->MONEY = GameManager::getInstance()->MONEY + getMoney();  ！！！！！在英雄的shoot函数里会有用！！！！！
+		//baseSprite->runAction(Animate::create(AnimationCache::getInstance()->getAnimation(getName() + "death")));
+		this->runAction(Sequence::create(FadeOut::create(1.0f)
+			, CallFuncN::create(CC_CALLBACK_0(BaseMonster::setVisible, this, false))
+			, NULL));
+	}
+}
+
+void BaseMonster::shoot(float dt)//每隔多久shoot一次 0.7
+{
+	auto instance = GameManager::getInstance();//
+	checkNearestMonster();
+	//char temp1[20];
+	//sprintf(temp1,"")
+	if (nearestMonster != NULL /*&& nearestMonster->getCurrHp() > 0*/)
+	{
+		spriteBullet = Sprite::create("");
+		addChild(spriteBullet);
+		//auto currBullet = ArrowTowerBullet();//生成弓箭
+		Point shootVector = nearestMonster->getParent()->getPosition() - this->getParent()->getPosition();//两点间 留下了geParent
+		//auto pVectr = currBullet->getPosition() - shootVector;//不知道干嘛
+		auto moveToAction = MoveTo::create(0.5f, shootVector);//我觉得是moveby啊
+		//currBullet->runAction(moveToAction);
+		//currBullet->shoot;//removeBullet!!
+		//currBullet->setPosition();//塔顶上
+		//currBullet = NULL;
+		//士兵只分红蓝向不同方向播放动作！！！弦儿！！！
+		spriteBullet->runAction(moveToAction);//, CallFuncN::create(CC_CALLBACK_0(BaseTower::removeBullet, this)), NULL));//BulletAction沿直线飞行 removeBullet判断击中并子弹消失
+		BaseMonster::removeBullet();
+	}
+}
+
+
+void BaseMonster::removeBullet()//攻击！  保证打中
+{
+	//bool isMissed = true;
+	auto instance = GameManager::getInstance();
+    //spriteBullet = Sprite::create("");
+	//addChild(spriteBullet);//add到哪了
+	
+	//bullet所在矩形区域
+	//auto bulletRect = Rect(spriteBullet->getPositionX() + this->towerBase->getPositionX() - spriteBullet->getContentSize().width / 2,
+		//spriteBullet->getPositionY() + this->towerBase->getPositionY() - spriteBullet->getContentSize().height / 2,
+		//spriteBullet->getContentSize().width,
+		//spriteBullet->getContentSize().height);//就矩形了就不用物理引擎了 rect区域真不太会啊
+	//if (this->getTowerColor() == 1)//red
+	//{
+		//auto monsterBlueVector = instance->monsterBlueVector;
+
+		//for (int j = 0; j < monsterBlueVector.size(); j++)
+		//{
+			//auto monster = monsterBlueVector.at(j);
+			//auto monsterRect = monster->baseSprite->getBoundingBox();
+			//if (monsterRect.intersectsRect(bulletRect) && monster->getState() != 7)
+			//{
+				auto currHp = nearestMonster->getCurrHp();
+
+				currHp = currHp - this->getForce();//受到攻击Hp减少
+
+				if (currHp <= 0)
+				{
+					currHp = 0;
+				}
+				nearestMonster->setCurrHp(currHp);
+
+				nearestMonster->getHpBar()->setPersentage((currHp / nearestMonster->getMaxHp()) * 100);//更新Hp
+				//monster->getHurt();     Wow!血腥场面！
+				//isMissed = false;//击中
+				if (currHp == 0)
+				{
+					nearestMonster->death();//消失啊 vector移除啊
+				}
+				//break;
+			//}
+		//}
+	//}
+	//else
+	//{
+		//auto monsterRedVector = instance->monsterRedVector;//啥意思啊
+
+		//for (int j = 0; j < monsterRedVector.size(); j++)
+		//{
+			//auto monster = monsterRedVector.at(j);
+			//auto monsterRect = monster->baseSprite->getBoundingBox();
+			//if (monsterRect.intersectsRect(bulletRect) && monster->getState() != 7)
+			//{
+				//auto currHp = monster->getCurrHp();
+
+				//currHp = currHp - this->getForce();//受到攻击Hp减少
+
+				//if (currHp <= 0)
+				//{
+					//currHp = 0;
+				//}
+				//monster->setCurrHp(currHp);
+
+				//monster->getHpBar()->setPersentage((currHp / monster->getMaxHp()) * 100);//更新Hp
+				//monster->getHurt();     Wow!血腥场面！
+				//isMissed = false;//击中
+				//if (currHp == 0)
+				//{
+					//monster->death();
+				//}
+				//break;
+			//}
+		//}
+	//}
+	//不管击不击中都消失
+	spriteBullet->removeFromParent();
+	/*if (isMissed)
+	{
+		//若未击中
+	}
+	else
+	{
+		this->removeFromParent();
+	}*/
+
+}
+
+/*void BaseMonster::removeBullet()//攻击！
+{
+	bool isMissed = true;
+	auto instance = GameManager::getInstance();
+	auto spriteBullet = Sprite::create("");
+	addChild(spriteBullet);
+
+	//bullet所在矩形区域
+	auto bulletRect = Rect(spriteBullet->getPositionX() + this->getPositionX() - spriteBullet->getContentSize().width / 2,
+		spriteBullet->getPositionY() + this->getPositionY() - spriteBullet->getContentSize().height / 2,
+		spriteBullet->getContentSize().width,//对不对呢 咱也不知道
+		spriteBullet->getContentSize().height);//就矩形了就不用物理引擎了
+	if (this->getMonsterColor() == 1)//red
+	{
+		auto monsterBlueVector = instance->monsterBlueVector;
+
+		for (int j = 0; j < monsterBlueVector.size(); j++)
+		{
+			auto monster = monsterBlueVector.at(j);
+			auto monsterRect = monster->baseSprite->getBoundingBox();
+			if (monsterRect.intersectsRect(bulletRect) && monster->getState() != 7)
+			{
+				auto currHp = monster->getCurrHp();
+
+				currHp = currHp - this->getForce();//受到攻击Hp减少
+
+				if (currHp <= 0)
+				{
+					currHp = 0;
+				}
+				monster->setCurrHp(currHp);
+
+				monster->getHpBar()->setPersentage((currHp / monster->getMaxHp()) * 100);//更新Hp
+				//monster->getHurt();     Wow!血腥场面！
+				isMissed = false;//击中
+				if (currHp == 0)
+				{
+					monster->death();//消失啊 vector移除啊
+				}
+				break;
+			}
+		}
+	}
+	else
+	{
+		auto monsterRedVector = instance->monsterRedVector;//啥意思啊
+
+		for (int j = 0; j < monsterRedVector.size(); j++)
+		{
+			auto monster = monsterRedVector.at(j);
+			auto monsterRect = monster->baseSprite->getBoundingBox();
+			if (monsterRect.intersectsRect(bulletRect) && monster->getState() != 7)
+			{
+				auto currHp = monster->getCurrHp();
+
+				currHp = currHp - this->getForce();//受到攻击Hp减少
+
+				if (currHp <= 0)
+				{
+					currHp = 0;
+				}
+				monster->setCurrHp(currHp);
+
+				monster->getHpBar()->setPersentage((currHp / monster->getMaxHp()) * 100);//更新Hp
+				//monster->getHurt();     Wow!血腥场面！
+				isMissed = false;//击中
+				if (currHp == 0)
+				{
+					monster->death();
+				}
+				break;
+			}
+		}
+	}
+	//不管击不击中都消失
+	this->removeFromParent();
+	/*if (isMissed)
+	{
+		//若未击中
+	}
+	else
+	{
+		this->removeFromParent();
+	}
+
+}
+
+void BaseMonster::shoot(float dt)
+{
+	auto instance = GameManager::getInstance();//
+	checkNearestMonster();
+	//char temp1[20];
+	//sprintf(temp1,"")
+	if (nearestMonster != NULL /*&& nearestMonster->getCurrHp() > 0)
+	{
+		//auto currBullet = ArrowTowerBullet();//生成弓箭
+		Point shootVector = nearestMonster->baseSprite->getPosition() - this->getPosition();//两点间 删了getParent
+		//auto pVectr = currBullet->getPosition() - shootVector;//不知道干嘛
+		auto moveToAction = MoveTo::create(0.5f, shootVector);//我觉得是moveby啊
+		//currBullet->runAction(moveToAction);
+		//currBullet->shoot;//removeBullet!!
+		//currBullet->setPosition();//塔顶上
+		//currBullet = NULL;
+		runAction(Sequence::create(moveToAction, CallFuncN::create(CC_CALLBACK_0(BaseTower::removeBullet, this)), NULL));//BulletAction沿直线飞行 removeBullet判断击中并子弹消失
+	}
+}*/
